e2202254ae738b056819cecaa53f13aadb0d7b59
Jeremy Olmsted-Thompson <jeremyot@google.com>
2020-07-09 23:43:16 -0700

    remove base image override

--- prepare-mcs-image.sh
+++ prepare-mcs-image.sh
@@ -7,6 +7,5 @@ KUBEROOT="${KUBEROOT:-$GOPATH/src/k8s.io/kubernetes}"
 
 kind build node-image --type docker \
   --image kindest/node:mc \
-  --kube-root $KUBEROOT \
-  --base-image kindest/base:v20200530-c3e2b553
+  --kube-root $KUBEROOT
 
